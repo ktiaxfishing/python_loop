7f537db8c8fbd76288488d5a9584b72c35212d19
s0ngkran <jinnawat8@gmail.com>
2021-11-20 14:11:30 +0700

    solved q3

--- homework1/q3.py
+++ homework1/q3.py
@@ -9,6 +9,38 @@
 .
 50
 '''
-for i in range(50):
-    if i %5:
-        print(i+1)
+
+# # while loop
+# a = 17
+# while True:
+#     print(a) # 17
+#     a = a+3 #20
+#     if a > 50:
+#         break
+
+# a = 17
+# while a<=50:
+#     print(a) # 17
+#     a = a+3 #20
+
+# # for loop syntax
+# for i in range(17, 51, 3):
+#     print(i)
+
+# for loop
+# for i in range(1000):
+#     # a = i+17
+#     if i < 17:
+#         continue
+#     if i > 50:
+#         break
+#     if i%3 == 2:
+#         print(i)
+
+# 1 %3 = 1
+# 2%3 = 2
+# 3%3  = 0
+# 4%3 = 1
+# 5%3 = 2
+# 6%6 = 0
+# 7%3 = 1
