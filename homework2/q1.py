9718a4c0aff8f57000c220379507008961d29e0c
s0ngkran <jinnawat8@gmail.com>
2021-11-21 12:29:13 +0700

    homework2

---
+++ homework2/q1.py
@@ -0,0 +1,17 @@
+# write a program to find the average value of a set of number.
+# gets the number until the input value equals zero
+# Then, calculates the average value and prints it out
+
+# >>> 3
+# >>> 6
+# >>> 9
+# >>> 0
+# >>> average = 6
+
+# >>> 10
+# >>> 20
+# >>> 0
+# >>> average = 15
+
+
+## code here
---
+++ homework2/q2.py
@@ -0,0 +1,21 @@
+# write a program to draw this image
+
+# a
+# aa
+# aaa
+# aaaa
+# aaaaa
+# aaaaaa
+# aaaaaaa
+# aaaaaaaa
+# aaaaaaaaa
+# aaaaaaaaaa
+# aaaaaaaaaaa
+# aaaaaaaaaaaa
+# aaaaaaaaaaaaa
+# aaaaaaaaaaaaaa
+# aaaaaaaaaaaaaaa
+# aaaaaaaaaaaaaaaa
+# aaaaaaaaaaaaaaaaa
+# aaaaaaaaaaaaaaaaaa
+# aaaaaaaaaaaaaaaaaaa
